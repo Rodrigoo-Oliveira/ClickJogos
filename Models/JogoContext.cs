947bf72e44f3542fcc118629deddc45dcf01d834
Rodrigoo-Oliveira <rodrigoodeoliveira.santos@gmail.com>
2022-05-12 22:31:26 -0300

    Initial commit

---
+++ Models/JogoContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ClickJogos.Models
+{
+    public class JogoContext : DbContext
+    {
+        public JogoContext(DbContextOptions<JogoContext>options) : base(options)
+        {
+
+        }
+
+        public DbSet<Jogo> Jogos {get;set;}
+    }
+}
